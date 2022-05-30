b876d50838f21f062a3d774e4693497fe9116512
DusanDjordjic <dukidjordjic@gmail.com>
2022-05-30 19:49:44 +0200

    Creating node default file

--- filetype.cpp
+++ filetype.cpp
@@ -25,4 +25,14 @@ void create_c(const std::string& file_name, const std::string& path)
 void create_node(const std::string& file_name, const std::string& path)
 {
     std::cout << "Create node file by name of " << file_name << " at " << path << std::endl;
+    std::string full_path;
+    if(*(path.end() - 1) != '/')
+        full_path = path + "/" + file_name + ".js";
+    else
+        full_path = path + file_name + ".js";
+
+    std::ofstream file(full_path);
+
+    file << "const http = require('http');\n\nconst hostname = '127.0.0.1';\nconst port = 3000;\n\nconst server = http.createServer((req, res) => {\n\tres.statusCode = 200;\n\tres.setHeader('Content-Type', 'text/plain');\n\tres.end('Hello World');\n});\n\nserver.listen(port, hostname, () => {\n\tconsole.log(`Server running at http://${hostname}:${port}/`);\n});" << std::endl;
+    file.close();
 }
